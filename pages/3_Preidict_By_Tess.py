dadf9eb8175075b0556b5d36cf169d4c6722464c
Salem Ahmed Salem <salemsalemahmed761@gmail.com>
2025-10-04 12:59:42 +0300

    Update 3_Preidict_By_Tess.py

--- pages/3_Preidict_By_Tess.py
+++ pages/3_Preidict_By_Tess.py
@@ -12,7 +12,8 @@ from sklearn.metrics import confusion_matrix, classification_report
 import matplotlib.pyplot as plt
 import seaborn as sns
 from io import BytesIO
-
+time.sleep(0.1)
+st.experimental_rerun()
 def display_prediction_result(result, model_name, idx):
     """Display individual prediction results with visualization"""
     
