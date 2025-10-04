62750a2765c4a9ca12ea3677d2e186c3699da949
Salem Ahmed Salem <salemsalemahmed761@gmail.com>
2025-10-04 12:11:35 +0300

    Update 1_Preidict_By_Kepler.py

--- pages/1_Preidict_By_Kepler.py
+++ pages/1_Preidict_By_Kepler.py
@@ -291,7 +291,7 @@ icons = {
 }
 
 with st.sidebar:
-    st.image("https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_1280.jpg", 
+    st.image("space.jpg", 
              use_container_width=True)
     st.title("🌌 Kepler Explorer")
     st.markdown("---")
